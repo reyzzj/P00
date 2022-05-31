bcaae09b10d3c73e7134b18f1e2bf312821c5320
reyzzj <21028514@myrp.edu.sg>
2022-05-31 13:16:54 +0800

    Testing 123

---
+++ c206_P00/src/c206_P00/Calculator.java
@@ -0,0 +1,28 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Zhe Jie
+ * Student ID: 21028514
+ * Class: W64L
+ * Date/Time created: Tuesday 31-05-2022 13:10
+ */
+
+package c206_P00;
+
+/**
+ * @author dd
+ *
+ */
+public class Calculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ c206_P00/src/c206_P00/CalculatorTest.java
@@ -0,0 +1,61 @@
+/*
+ * I declare that this code was written by me. 
+ * I do not copy or allow others to copy my code. 
+ * I understand that copying code is considered as plagiarism.
+ * 
+ * Student Name: Zhe Jie
+ * Student ID: 21028514
+ * Class: W64L
+ * Date/Time created: Tuesday 31-05-2022 13:11
+ */
+
+package c206_P00;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * @author dd
+ *
+ */
+public class CalculatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
